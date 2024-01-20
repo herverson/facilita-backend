926c580f5aec4b5fc02ccf85f6a183ae81b47ff2
Herverson de Sousa <herverson.sousa@gmail.com>
2024-01-20 11:49:15 -0300

    dot env

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports = {
   client: 'pg',
   connection: {
-    host: 'localhost',
-    user: 'postgres',
-    password: '1234',
-    database: 'postgres',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
 };
